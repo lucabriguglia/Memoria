895179e8e0476a12828c596d0c0e559295797011
luca.briguglia <luca.briguglia@outlook.com>
2026-05-09 11:46:47 +0100

    Fix typo in `EventPropertyFilter` initialization in `TestAggregateIdWithFilter` model test.

--- tests/Memoria.EventSourcing.Store.Tests/Models/Aggregates/TestAggregateIdWithFilter.cs
+++ tests/Memoria.EventSourcing.Store.Tests/Models/Aggregates/TestAggregateIdWithFilter.cs
@@ -5,5 +5,5 @@ namespace Memoria.EventSourcing.Store.Tests.Models.Aggregates;
 public class TestAggregateIdWithFilter(string testAggregateId, string something) : IAggregateId<TestAggregate1>
 {
     public string Id => $"test-aggregate-with-filter:{testAggregateId}:{something}";
-    public string[]? EventPropertyFilter { get; } = [$"Something2={something}]"];
+    public string[]? EventPropertyFilter { get; } = [$"Something2={something}"];
 }
